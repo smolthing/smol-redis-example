a4eea218abc723d0a8242f5ed3b4802fbe2894e8
mysmolthing <mysmolthing@gmail.com>
2024-05-26 00:58:47 +0800

    Add a get /data endpoint

--- src/main/java/com/smol/redis/smol_redis/MainVerticle.java
+++ src/main/java/com/smol/redis/smol_redis/MainVerticle.java
@@ -1,17 +1,20 @@
 package com.smol.redis.smol_redis;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
-
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
+    Router router = Router.router(vertx);
+    router.get("/data").respond(ctx->
+      Future.succeededFuture(new JsonObject().put("data", "value"))
+    );
+
+    vertx.createHttpServer().requestHandler(router).listen(8000, http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
